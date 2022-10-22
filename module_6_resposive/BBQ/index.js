f018d9e6c5731a155a5c9ae73fee49f6e3872a5c
FawziaAkbari <57459554+FawziaAkbari@users.noreply.github.com>
2022-10-22 06:38:05 +0430

    adding the responsive challenges

---
+++ module_6_resposive/BBQ/index.js
@@ -0,0 +1,19 @@
+let info = document.getElementById("nameEmail")
+info.addEventListener("submit", event => {
+	event.preventDefault()
+
+	let ourFormData = new FormData(event.target)
+
+	const name = ourFormData.get("firstName")
+
+	const textUpdate = `
+						<h2>Congratulations, ${name}!</h2>
+
+                    <p>You're on your way to becoming a BBQ Master!</p>
+                    
+                    <p class="fine-print">We'll never share your information without your permission</p>
+	`
+
+	const register = document.getElementById("contents")
+	register.innerHTML = textUpdate
+})
